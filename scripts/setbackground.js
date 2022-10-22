47186cca2b51c16c299ed4ebf40482aaa3ffaf61
Eric Winebrenner <ejwinebrenner@gmail.com>
2022-10-22 19:44:56 -0400

    testing background image links

--- scripts/setbackground.js
+++ scripts/setbackground.js
@@ -25,65 +25,65 @@ export const setBackground = (threat, type) => {
     switch(check) {
         // BP
         case "double spawn":
-            image = `url('../assets/bp_doubleSpawn-removebg.png')`;
+            image = `url('./assets/bp_doubleSpawn-removebg.png')`;
             posX = '-10px';
             posY = '180px';
             break;
         case "walker":
-            image = `url('../assets/bp_walker-removebg1.PNG')`;
+            image = `url('./assets/bp_walker-removebg1.PNG')`;
             posX = '-100px';
             posY = '130px';
             break;
         case "runner":
-            image = `url('../assets/bp_runner-removebg.png')`;
+            image = `url('./assets/bp_runner-removebg.png')`;
             posX = '-60px';
             posY = '170px';
             break;
         case "fatty":
-            image = `url('../assets/bp_fatty-removebg.png')`;
+            image = `url('./assets/bp_fatty-removebg.png')`;
             posX = '-150px';
             posY = '170px';
             break;
         case "necromancer":
-            image = `url('../assets/bp_necro-removebg.png')`;
+            image = `url('./assets/bp_necro-removebg.png')`;
             posX = '120px';
             posY = '150px';
             break;
         case "abomination":
-            image = `url('../assets/bp_abom-removebg.png')`;
+            image = `url('./assets/bp_abom-removebg.png')`;
             posX = '-80px';
             posY = '100px';
             size = '140%';
             break;
         case "walker activation":
-            image = `url('../assets/bp_walkerActivate-removebg.png')`;
+            image = `url('./assets/bp_walkerActivate-removebg.png')`;
             posX = '-60px';
             posY = '100px';
             size = '120%';
             break;
         case "runner activation":
-            image = `url('../assets/bp_runnerActivate-removebg.png')`;
+            image = `url('./assets/bp_runnerActivate-removebg.png')`;
             posX = '-20px';
             posY = '300px';
             size = '120%';
             break;
         case "fatty activation":
-            image = `url('../assets/bp_fattyActivate-removebg.png')`;
+            image = `url('./assets/bp_fattyActivate-removebg.png')`;
             posY = '200px';
             break;
         // GH
         case "enter the horde":
-            image = `url('../assets/gh_enterthehorde-removebg.png')`;
+            image = `url('./assets/gh_enterthehorde-removebg.png')`;
             posX = '-10px';
             posY = '150px';
             break;
         case "orc walker":
-            image = `url('../assets/gh_walker-removebg.png')`;
+            image = `url('./assets/gh_walker-removebg.png')`;
             posX = '90px';
             posY = '180px';
             break;
         case "orc runner":
-            image = `url('../assets/gh_runner-removebg.png')`;
+            image = `url('./assets/gh_runner-removebg.png')`;
             posX = '-90px';
             posY = '190px';
             break;
@@ -95,62 +95,62 @@ export const setBackground = (threat, type) => {
             break;
         case "orc necromancer":
             //NOTE: need image
-            image = `url('../assets/')`;
+            image = `url('./assets/')`;
             posX = '120px';
             posY = '150px';
             break;
         case "orc abomination":
             //NOTE: need image
-            image = `url('../assets/')`;
+            image = `url('./assets/')`;
             posX = '-80px';
             posY = '100px';
             size = '140%';
             break;
         // WB
         case "wolvz":
-            image = `url('../assets/wb_wolf-removebg.png')`;
+            image = `url('./assets/wb_wolf-removebg.png')`;
             posX = '-65px';
             posY = '220px';
             break;
         case "wolfbomination":
-            image = `url('../assets/wb_abom-removebg.png')`;
+            image = `url('./assets/wb_abom-removebg.png')`;
             posX = '40px';
             posY = '180px';
             size = '140%';
             break;
         // NR
         case "swarm of ratz":
-            image = `url('../assets/nr_rats-removebg.png')`;
+            image = `url('./assets/nr_rats-removebg.png')`;
             posX = '-50px';
             posY = '150px';
             break;
         case "spectral walker":
-            image = `url('../assets/nr_specter-removebg.png')`;
+            image = `url('./assets/nr_specter-removebg.png')`;
             posX = '-80px';
             posY = '100px';
             size = '140%';
             break;
         case "necromantic dragon":
-            image = `url('../assets/nr_dragon-removebg.png')`;
+            image = `url('./assets/nr_dragon-removebg.png')`;
             posX = '-80px';
             posY = '100px';
             size = '140%';
             break;
         // FF
         case "tainted walker":
-            image = `url('../assets/ff_taintedWalker-removebg.png')`;
+            image = `url('./assets/ff_taintedWalker-removebg.png')`;
             posX = '-10px';
             posY = '150px';
             break;
         case "tainted abomination":
-            image = `url('../assets/ff_abom-removebg.png')`;
+            image = `url('./assets/ff_abom-removebg.png')`;
             posX = '-80px';
             posY = '100px';
             size = '140%';
             break;
         // MC
         case "murder of crow":
-            image = `url('../assets/mc_crows-removebg.png')`;
+            image = `url('./assets/mc_crows-removebg.png')`;
             posX = '-120px';
             posY = '150px';
             break;
